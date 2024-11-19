7d19590e78cdf63a0bed78d0055026518f987a1d
emanuelmartinsgit123 <emanuelsonmartin009@gmail.com>
2024-11-19 10:25:24 -0300

    Create index.php
    project index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+php
+Copiar código
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Site com PHP</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Bem-vindo ao meu site!</h1>
+    </header>
+
+    <main>
+        <p>Essa é a página inicial.</p>
+        <p><a href="form.php">Clique aqui para preencher um formulário.</a></p>
+    </main>
+
+    <footer>
+        <p>Desenvolvido com PHP e HTML.</p>
+    </footer>
+</body>
+</html>
